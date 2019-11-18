3e574ac5feefb035ad550c36449e5e92ff951155
Robert <rwojcieszek97@gmail.com>
2019-11-19 00:04:32 +0100

    Działająca z gifem

--- src/main/java/toe/tac/tic/Main.java
+++ src/main/java/toe/tac/tic/Main.java
@@ -7,26 +7,16 @@ public class Main {
 
 	static StartJFrame startJFrame = new StartJFrame();
 	static GameFrame gameFrame = new GameFrame();
-<<<<<<< HEAD
 	static WinFrame winFrame = new WinFrame();
-=======
-//	static NickNameFrame nickNameFrame = new NickNameFrame();
->>>>>>> 7ec6372499c6fb8d132affb26561fcf6d1db2428
 
     public static void main(String[] args) {
 
         EventQueue.invokeLater(() -> {
         	startJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			startJFrame.setVisible(false);
+			startJFrame.setVisible(true);
         	gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			gameFrame.setVisible(false);
-<<<<<<< HEAD
 			winFrame.setVisible(false);
-=======
-
-//			nickNameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//			nickNameFrame.setVisible(true);
->>>>>>> 7ec6372499c6fb8d132affb26561fcf6d1db2428
         });
     }
 }
--- src/main/java/toe/tac/tic/StartJFrame.java
+++ src/main/java/toe/tac/tic/StartJFrame.java
@@ -14,11 +14,8 @@ public class StartJFrame extends JFrame{
 		StartPanel startPanel = new StartPanel();
 		add(startPanel);
 
-<<<<<<< HEAD
+
 		Image icon = Toolkit.getDefaultToolkit().getImage("unnamed.png");  
-=======
-		Image icon = Toolkit.getDefaultToolkit().getImage("/Users/kavit/Downloads/tic-tac-toe (1).png");
->>>>>>> 7ec6372499c6fb8d132affb26561fcf6d1db2428
 		setIconImage(icon);
 	}
 }
